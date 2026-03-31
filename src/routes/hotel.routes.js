cb80b3bec764f55d1141ac1e39e6c641710c469d
pietech2026-png <rhythm.shesha@gmail.com>
2026-03-31 19:51:34 +0530

    Update project: remove backend files and consolidate flutter app

--- src/routes/hotel.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const hotelController = require("../controllers/hotel.controller");
-
-router.get("/:location", hotelController.getHotels);
-router.post("/", hotelController.addHotel);
-
-module.exports = router;
